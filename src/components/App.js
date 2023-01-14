10c85062b7770934439eb108b662cc052c3bfc0e
cy <cagdasyonder@hotmail.com>
2023-01-14 14:43:57 +0300

    added index.js and App.js as a starting point

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>Hello, world!</div>
+    );
+  }
+}
+
+export default App;
